d722c85d48e65f60338f79e6de8b94d26f512872
Damiano Raab <DamianoRaab11@gmail.com>
2019-01-18 11:33:45 -0500

    connection and db created

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+
+insert into burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Cheese Burger", FALSE, null),
+(null, "Bacon Burger", FALSE, null),
+(null, "Quater Pounder Burger", FALSE, null),
+(null, "Fire Burger", FALSE, null),
+(null, "Triple Decker Burger", FALSE, null),
+(null, "Impossible Burger", FALSE, null);
