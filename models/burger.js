ae889e98d7a916151963464c6125c1b462893b45
Damiano Raab <DamianoRaab11@gmail.com>
2019-01-18 11:59:43 -0500

    files

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+
+    all:function(br) {
+        orm.all('burgers', function(res){
+            br(res);
+        });
+    },
+
+    create: function(cols, vals, br) {
+        orm.create('burgers', cols, vals, function(res){
+            br(res);
+        });
+    },
+
+    update: function(objColVals, condition, br) {
+        orm.update('burgers', objColVals, condition, function(res){
+            br(res);
+        });
+    },
+
+};
+
+module.exports = burger;
